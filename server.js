c510d83f165f3d5dbe8ad8a5f1619b607e77a9bf
Thomas Castleman <thomas7rex@gmail.com>
2018-03-18 10:21:02 -0400

    Error handling

--- server.js
+++ server.js
@@ -12,14 +12,40 @@ app.use('/', express.static('views'));
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
-var school_start = moment('2017-08-24');
-var school_end = moment('2018-06-01');
+// 2017-18 schoolyear parameters
+var current_start = moment('2017-08-24');
+var current_end = moment('2018-06-01');
 
-var interval = (school_end - school_start) * 0.00001;
+// 2018 summer parameters
+var summer_start = moment('2018-06-02');
+var summer_end = moment('2018-08-22');
 
-// get percentage of year complete
+var needs_maintenance = false;
+
+var interval = (current_end - current_start) * 0.00001;
+
+// calculate a percentage into the current time interval
+function calcPercentage() {
+	return (moment() - current_start) / (current_end - current_start) * 100;
+}
+
+// get percentage of year complete, switch interval if necessary
 function getPercentage() {
-	return (moment() - school_start) / (school_end - school_start) * 100;
+	var p = calcPercentage();
+	if (p > 100) {
+		if (moment().isBefore(summer_end)) {
+			// switch to summer data
+			current_start = summer_start;
+			current_end = summer_end;
+
+			return calcPercentage();
+		} else {
+			needs_maintenance = true;
+			return 0;
+		}
+	} else {
+		return p;
+	}
 }
 
 // establish socket for dynamic updating
@@ -31,10 +57,11 @@ io.on('connection', function(socket) {
 });
 
 app.get('/', function(req, res) {
-	res.render('client.html', { 
+	res.render('client.html', {
 		percentage: getPercentage().toFixed(3),
-		start_date: school_start.format('M/D/YYYY'),
-		end_date: school_end.format('M/D/YYYY')
+		start_date: current_start.format('M/D/YYYY'),
+		end_date: current_end.format('M/D/YYYY'),
+		needs_maintenance: needs_maintenance
 	});
 });
 
